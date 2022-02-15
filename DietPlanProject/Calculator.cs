3a1bb9b51b4c53b98a27b766932f3b5b47de6499
Shannon Birkley <shannonbirkley@gmail.com>
2022-02-14 19:34:48 -0500

    Initial commit

---
+++ DietPlanProject/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietPlanProject
+{
+    public class Calculator
+    {
+        public double height;
+        public double weight;
+        public int age;
+        public double activityLevel;
+
+        //Mifflin St. Jeor method of calculating TDEE
+        public double MiffStJeor(double weight, double height, int age, double activityLevel)
+        {
+            double basalMetabolicRate = (10 * weight) + (6.25 * height) + (5 * age) - 161;
+            return basalMetabolicRate;
+
+            double energyExpidenture = basalMetabolicRate * activityLevel;
+            return energyExpidenture;
+
+            Console.WriteLine($"Your TDEE is { energyExpidenture }.");
+
+            //need to round energyExpidenture
+        }
+    }
+}
